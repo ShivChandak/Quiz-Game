d35e3d98ff34cd46db326fc4a4b8049b95a98ac3
ShivChandak <94048313+ShivChandak@users.noreply.github.com>
2022-04-27 17:19:32 +0530

    Update forms.py

--- quizgame/users/forms.py
+++ quizgame/users/forms.py
@@ -9,7 +9,7 @@ class NewUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("first_name", "email", "password1", "password2")
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
