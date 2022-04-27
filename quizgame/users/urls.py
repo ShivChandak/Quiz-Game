2bebd717fe827c5c1b5c1cb65f3ec89cfd7e9ca1
ShivChandak <94048313+ShivChandak@users.noreply.github.com>
2022-04-27 17:31:02 +0530

    Update urls.py

--- quizgame/users/urls.py
+++ quizgame/users/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("register", views.register_request, name="register"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
     path("ques2", views.ques2, name="ques2"),
     path("ques3", views.ques3, name="ques3"),
     path("result", views.result, name="result"),
